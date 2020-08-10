a5ed58b29d0d8427fa46571ee05615ebe504bf45
Artur Yumagulov <zico.13288@gmail.com>
2020-08-10 23:17:29 +0300

    create login_page_test

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link, timeout=3)
+    page = MainPage(browser, link, timeout=10)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
